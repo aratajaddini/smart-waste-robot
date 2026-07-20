b936021c5ba8375fd92a5bbe9d6754acab44eb5d
abbas <abbaslotfi1570815224@gmail.com>
2026-07-20 22:18:19 +0330

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,37 @@
+import io
+from functools import lru_cache
+from PIL import Image
+import numpy as np
+
+@lru_cache(maxsize=1)
+def _get_model():
+    
+    from ultralytics import YOLO
+    return YOLO("weights/best.pt",task="classify")
+
+def run_inference(image_bytes: bytes) -> dict:
+    
+    model = _get_model()
+    
+    
+    img_pil = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    
+    
+    img_np = np.array(img_pil)
+    
+   
+    results = model(img_np, verbose=False)[0]
+    probs = results.probs
+    names = model.names  
+
+    
+    scores = {
+        names[i]: round(float(probs.data[i]), 4)
+        for i in names
+    }
+
+    return {
+        "top_class": names[int(probs.top1)],
+        "confidence": round(float(probs.top1conf), 4),
+        "scores": scores,
+    }
